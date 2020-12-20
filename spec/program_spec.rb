7fcefa7fe524f2982aefee746f830ae39365475b
Fernando Gruning <fernando@bejamas.io>
2020-12-20 12:57:06 -0400

    SCE-1-1-2 passed

--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -6,4 +6,8 @@ describe Program do
   it 'SCE-1-1-1: OrderAndDedup([9,6,3,2,12]) debe retornar [2,3,6,9,12]' do
     expect(program.OrderAndDedup([9,6,3,2,12])).to eq [2,3,6,9,12]
   end
+
+  it 'SCE-1-1-2: OrderAndDedup([9,8,7,6,5,4,3,2,1]) debe retornar [1,2,3,4,5,6,7,8,9]' do
+    expect(program.OrderAndDedup([9,8,7,6,5,4,3,2,1])).to eq [1,2,3,4,5,6,7,8,9]
+  end
 end
